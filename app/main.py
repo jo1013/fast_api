1c24d4e341f4f042da0ceb4ca23e85ec7e6c77d9
small-ticket-sawyer <tkdah0@gmail.com>
2021-06-14 16:54:02 +0900

    210614_16:53

--- app/main.py
+++ app/main.py
@@ -24,18 +24,4 @@ def read_item(item_id: int, q: Optional[str] = None):
 
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}from typing import Optional
-
-from fastapi import FastAPI
-
-app = FastAPI()
-
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+    return {"item_name": item.name, "item_id": item_id}
