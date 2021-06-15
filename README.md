b9476b811af6be917fd4e6f72255bf7f1ed9da23
small-ticket-sawyer <tkdah0@gmail.com>
2021-06-15 14:26:37 +0900

    210615_14:26

--- README.md
+++ README.md
@@ -1,3 +1,48 @@
 # fast_api
 
-docker hub 주소 jo1013/fast_api:0.01
+### docker hub 주소 jo1013/fast_api:0.01
+
+* docker container에 필요한 추가 라이브러리가 있을 경우 (컨테이너실행중) 설치 후 커밋 docker hub에 푸시하면 된다. 
+
+### 도커 컨테이너 실행
+```
+$docker run -i jo1013/fast_api:0.01
+```
+### docker docker 터미널 
+```
+$docker exec -it b83b5afe25bd bash
+$docker exec -it [CONTAINER ID] bash
+```
+
+### docker cotainer ID check
+
+``` 
+$docker ps
+```
+
+
+### docker commit 
+
+```
+$ docker commit 977bd1a14aa6 jo1013/fastapi:0.01
+$ docker commit [container_id] [image]:[tag]
+```
+
+
+### docker push 
+
+$ docker push jo1013/fastapi:0.01
+$ docker push [images]:[tag]
+
+### docker bash (open port)
+```
+$ docker run -it --rm -p [로컬포트]:[컨테이너포트] -v [local 경로]:[컨테이너경로] [image]:[tag]
+$ docker run -it --rm -p 8888:8888 -v `pwd`:/home jo1013/fastapi:0.02
+```
+
+### docker bash
+```
+$ jupyter notebook --allow-root --ip=0.0.0.0 --port=8888 --no-browser
+```
+ 
+ 
---
+++ app/main2.py
@@ -0,0 +1,59 @@
+import webbrowser
+import requests
+import json
+from typing import Optional
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+import csv
+import pandas as pd
+from matplotlib import pyplot
+
+import pandas as pd
+from sklearn.base import BaseEstimator, TransformerMixin
+from fastapi.responses import HTMLResponse
+
+from bs4 import BeautifulSoup
+
+seoul_location = motor_acc[['위도', '경도']]
+
+motor_acc = pd.read_csv('17_19_motorcycle.csv',encoding='cp949')
+map = folium.Map(location =  [37.5642,127.0016985,], zoom_start =12)
+
+for a in seoul_location.index:
+    latitude = seoul_location.loc[a,"위도"]
+    longtitude = seoul_location.loc[a,"경도"]
+    tooltip = "사고!"
+    folium.Marker([latitude, longtitude], popup = '<i>위험</i>', tooltip =tooltip).add_to(map)
+
+map.save('map.html')
+
+
+
+
+
+app = FastAPI()
+
+
+
+
+
+
+
+f = open("map.html", "r", encoding="utf8")
+soup = BeautifulSoup(f, 'html.parser')
+for link in soup.find_all('a'):
+    print(link.get('href'))
+
+
+
+@app.get("/")
+def read_root():
+
+motor_acc = pd.read_csv('/home/restapi/17_19_motorcycle.csv',encoding='cp949')
+
+
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
